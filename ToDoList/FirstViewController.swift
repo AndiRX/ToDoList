f28f539c7affd088be4128c9c1a05d6801ee3054
Petr <petr@petrs-air.localdomain>
2017-12-16 20:32:47 +0100

    first commit before coding delete button

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        if let tempItems = itemsObject as? [String] {
+            items = tempItems
+        }
+        self.tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+    
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
     }
-
+    
+   public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    
+    @IBAction func deleteBtnPressed(_ sender: Any) {
+        
+        
+        
+    }
+    
 
 }
 
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func addBtnPressed(_ sender: Any) {
+        
+//        let itemsObject = UserDefaults.standard.object(forKey: "items")
+//        var items = NSMutableArray()
+//        if let _ = itemsObject as? NSMutableArray {
+//            items.addObjects(from: [textField.text!])
+//        } else {
+//            items = [textField.text!]
+//        }
+//
+//        UserDefaults.standard.set(items, forKey: "items")
+
+        let itemsObject = UserDefaults.standard.stringArray(forKey: "items")
+        var items = [String]()
+        if let _ = itemsObject {
+            items = itemsObject!
+            items.append(textField.text!)
+            print("items appended")
+            print(items)
+        } else {
+            items = [textField.text!]
+            print("new items array created")
+            print(items)
+        }
+        UserDefaults.standard.set(items, forKey: "items")
+        
+        textField.text = ""
+        
+    }
+    
 
 }
 
